ad9a42f043a4f0985340c04d225adc0c76603774
Abhijit17 <puriabhijit000@gmail.com>
2021-07-09 18:51:57 +0530

    started mental health section

---
+++ app/src/main/java/com/example/prana/mental_health.java
@@ -0,0 +1,31 @@
+package com.example.prana;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class mental_health extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mental_health);
+    }
+
+    public void gotoMemes(View view)
+    {
+
+    }
+
+    public void gotoMusic(View view)
+    {
+
+    }
+
+    public void gotoMotivationalQuotes(View view)
+    {
+
+    }
+}
